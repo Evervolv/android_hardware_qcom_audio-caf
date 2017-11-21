3b98c31c928ee038afd8594005a7a053651d9b49
Wei Wang <wvw@google.com>
2017-11-21 14:51:20 -0800

    audio-hal: use powerHAL 1.2 to do performance hint
    1) Add PowerHAL 1.2 client
    2) call PowerHAL powerhint on msm8998 instead of perfd
    
    Test: Play audio and see powerhint handled
    Bug: 62041945
    Change-Id: I33ab8eaa7a3a3a953fb6531e0864b4ed62387ba2
    (cherry picked from commit f4837d5bbe73df6f33aee2d0f733f72cc635de6a)

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -70,6 +70,7 @@
 #include <audio_effects/effect_ns.h>
 #include <audio_utils/format.h>
 #include "audio_hw.h"
+#include "audio_perf.h"
 #include "platform_api.h"
 #include <platform.h>
 #include "audio_extn.h"
@@ -2554,6 +2555,7 @@ int start_input_stream(struct stream_in *in)
     uc_info->out_snd_device = SND_DEVICE_NONE;
 
     list_add_tail(&adev->usecase_list, &uc_info->list);
+    audio_streaming_hint_start();
     audio_extn_perf_lock_acquire(&adev->perf_lock_handle, 0,
                                  adev->perf_lock_opts,
                                  adev->perf_lock_opts_size);
@@ -2656,14 +2658,17 @@ int start_input_stream(struct stream_in *in)
     check_and_enable_effect(adev);
 
 done_open:
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     ALOGD("%s: exit", __func__);
     enable_gcov();
     return ret;
 
 error_open:
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     stop_input_stream(in);
+
 error_config:
     adev->active_input = get_next_active_input(adev);
     /*
@@ -3155,6 +3160,7 @@ int start_output_stream(struct stream_out *out)
 
     list_add_tail(&adev->usecase_list, &uc_info->list);
 
+    audio_streaming_hint_start();
     audio_extn_perf_lock_acquire(&adev->perf_lock_handle, 0,
                                  adev->perf_lock_opts,
                                  adev->perf_lock_opts_size);
@@ -3361,7 +3367,7 @@ int start_output_stream(struct stream_out *out)
                 goto error_open;
         }
     }
-
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     ALOGD("%s: exit", __func__);
 
@@ -3382,6 +3388,7 @@ int start_output_stream(struct stream_out *out)
     enable_gcov();
     return ret;
 error_open:
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     stop_output_stream(out);
 error_config:
